9ca331b2dd7574c440dbb0dd1b6dced5b830ac84
AnhBH <jangkyavn@gmail.com>
2021-07-11 22:13:11 +0700

    update

--- Services/Repositories/Implimentations/TienIch/NhatKyTruyCapService.cs
+++ Services/Repositories/Implimentations/TienIch/NhatKyTruyCapService.cs
@@ -1,8 +1,26 @@
-﻿using Services.Repositories.Interfaces.TienIch;
+﻿using ManagementServices.Helper;
+using Services.Helper.Params.TienIch;
+using Services.Repositories.Interfaces.TienIch;
+using Services.ViewModels.TienIch;
+using System.Threading.Tasks;
 
 namespace Services.Repositories.Implimentations.TienIch
 {
     public class NhatKyTruyCapService : INhatKyTruyCapService
     {
+        public Task<bool> DeleteAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PagedList<NhatKyTruyCapViewModel>> GetAllPagingAsync(NhatKyTruyCapParams @params)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<NhatKyTruyCapViewModel> GetByIdAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
