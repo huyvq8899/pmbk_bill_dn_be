24e73371a485e937c883f64a4cd9cf871b75fdb7
Thu Ngô <thunth.1710@gmail.com>
2021-07-19 10:57:53 +0700

    add-migration

---
+++ DLL/Migrations/20210717123526_add-hien-thi-khi-cong-gop.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class addhienthikhiconggop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HienThiKhiCongGop",
+                table: "TruongDuLieus",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HienThiKhiCongGop",
+                table: "TruongDuLieus");
+        }
+    }
+}
