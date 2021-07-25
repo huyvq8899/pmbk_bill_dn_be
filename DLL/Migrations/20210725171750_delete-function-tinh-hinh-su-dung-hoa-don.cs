a739a7476f87c2bf2141f7775253f65e046b6832
Thu Ngô <thunth.1710@gmail.com>
2021-07-26 00:20:03 +0700

    commit all code

---
+++ DLL/Migrations/20210725171750_delete-function-tinh-hinh-su-dung-hoa-don.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class deletefunctiontinhhinhsudunghoadon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Functions",
+                keyColumn: "FunctionName",
+                keyValue: "TinhHinhSuDungHoaDon"
+
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
