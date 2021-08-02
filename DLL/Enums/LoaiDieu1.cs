726e3b7087d5dba3737e5a7d9861c5109b46e517
AnhBH <jangkyavn@gmail.com>
2021-08-02 08:14:44 +0700

    update

--- DLL/Enums/LoaiDieu1.cs
+++ DLL/Enums/LoaiDieu1.cs
@@ -1,11 +1,18 @@
-﻿namespace DLL.Enums
+﻿using System.ComponentModel;
+
+namespace DLL.Enums
 {
     public enum LoaiDieu1
     {
+        [Description("Máy tính: ")]
         MayTinh = 1,
+        [Description("Máy in: ")]
         MayIn = 2,
+        [Description("Chứng thư in: ")]
         ChungThuSo = 3,
+        [Description("Thiết bị: ")]
         ThietBi = 4,
+        [Description("Phần mềm ứng dụng: ")]
         PhanMemUngDung = 5
     }
 }
--- Services/Repositories/Implimentations/DanhMuc/QuyetDinhApDungHoaDonService.cs
+++ Services/Repositories/Implimentations/DanhMuc/QuyetDinhApDungHoaDonService.cs
@@ -7,10 +7,14 @@ using ManagementServices.Helper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
 using Services.Helper;
 using Services.Helper.Params.DanhMuc;
 using Services.Repositories.Interfaces.DanhMuc;
 using Services.ViewModels.DanhMuc;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,13 +27,19 @@ namespace Services.Repositories.Implimentations.DanhMuc
     {
         private readonly Datacontext _db;
         private readonly IMapper _mp;
+        private readonly IHoSoHDDTService _hoSoHDDTService;
         private readonly IHostingEnvironment _hostingEnvironment;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public QuyetDinhApDungHoaDonService(Datacontext datacontext, IMapper mapper, IHostingEnvironment hostingEnvironment, IHttpContextAccessor httpContextAccessor)
+        public QuyetDinhApDungHoaDonService(Datacontext datacontext,
+            IMapper mapper,
+            IHoSoHDDTService hoSoHDDTService,
+            IHostingEnvironment hostingEnvironment,
+            IHttpContextAccessor httpContextAccessor)
         {
             _db = datacontext;
             _mp = mapper;
+            _hoSoHDDTService = hoSoHDDTService;
             _hostingEnvironment = hostingEnvironment;
             _httpContextAccessor = httpContextAccessor;
         }
@@ -51,6 +61,206 @@ namespace Services.Repositories.Implimentations.DanhMuc
             _db.QuyetDinhApDungHoaDons.Remove(entity);
             var result = await _db.SaveChangesAsync() > 0;
             return result;
+        }
+
+        public async Task<FileReturn> ExportFileAsync(string id, DinhDangTepMau dinhDangTepMau)
+        {
+            QuyetDinhApDungHoaDonViewModel model = await GetByIdAsync(id);
+
+            HoSoHDDTViewModel hoSoHDDTVM = await _hoSoHDDTService.GetDetailAsync();
+
+            Document doc = new Document();
+
+            string srcPath = Path.Combine(_hostingEnvironment.WebRootPath, $"docs/QuyetDinhApDungHDDT/Quyet_dinh_ap_dung_HDDT.docx");
+            string destPath = Path.Combine(_hostingEnvironment.WebRootPath, $"temp");
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+            doc.LoadFromFile(srcPath);
+            Section section = doc.Sections[0];
+
+            doc.Replace("<TenDonVi>", hoSoHDDTVM.TenDonVi ?? string.Empty, true, true);
+            doc.Replace("<DiaChi>", hoSoHDDTVM.DiaChi ?? string.Empty, true, true);
+            doc.Replace("<dd>", model.NgayQuyetDinh.Value.ToString("dd") ?? string.Empty, true, true);
+            doc.Replace("<mm>", model.NgayQuyetDinh.Value.ToString("MM") ?? string.Empty, true, true);
+            doc.Replace("<yyyy>", model.NgayQuyetDinh.Value.ToString("yyyy") ?? string.Empty, true, true);
+
+            #region Căn cứ để ban hành QĐ
+            ListStyle lsCanCuDeBanHanhQD = new ListStyle(doc, ListType.Bulleted);
+            lsCanCuDeBanHanhQD.Name = "lsCanCuDeBanHanhQD";
+            lsCanCuDeBanHanhQD.Levels[0].BulletCharacter = "\x002D";
+            lsCanCuDeBanHanhQD.Levels[0].ParagraphFormat.AfterSpacing = 6;
+            lsCanCuDeBanHanhQD.Levels[0].CharacterFormat.FontName = "Times New Roman";
+            lsCanCuDeBanHanhQD.Levels[0].ParagraphFormat.LeftIndent = 30F;
+            doc.ListStyles.Add(lsCanCuDeBanHanhQD);
+
+            string[] canCuDeBanHanhQDs = model.CanCuDeBanHanhQuyetDinh.Split("\n");
+            foreach (string item in canCuDeBanHanhQDs)
+            {
+                Paragraph par = section.AddParagraph();
+                par.AppendText(item);
+                par.ListFormat.ApplyStyle(lsCanCuDeBanHanhQD.Name);
+            }
+            #endregion
+
+            #region Quyết định
+            Paragraph paraQD = section.AddParagraph();
+            paraQD.Format.HorizontalAlignment = HorizontalAlignment.Center;
+            TextRange trQD = paraQD.AppendText("QUYẾT ĐỊNH");
+            trQD.CharacterFormat.Bold = true;
+            trQD.CharacterFormat.FontSize = 14;
+            section.AddParagraph();
+            #endregion
+
+            #region Điều 1
+            Paragraph paraDieu1 = section.AddParagraph();
+            paraDieu1.Format.AfterSpacing = 6;
+            TextRange trDieu1 = paraDieu1.AppendText($"Điều 1. {model.Dieu1}");
+            trDieu1.CharacterFormat.Bold = true;
+
+            Paragraph paraHeThongThietBi = section.AddParagraph();
+            paraHeThongThietBi.AppendText("1. Hệ thống thiết bị:");
+            paraHeThongThietBi.Format.BeforeSpacing = 6;
+            paraHeThongThietBi.Format.AfterSpacing = 6;
+
+            var list123 = Enum.GetValues(typeof(LoaiDieu1))
+               .Cast<LoaiDieu1>()
+               .Where(x => x >= LoaiDieu1.MayTinh && x <= LoaiDieu1.ChungThuSo)
+               .Select(v => new EnumModel
+               {
+                   Value = v,
+                   Name = v.GetDescription()
+               })
+               .ToList();
+
+            ListStyle listLoaiDieu123 = new ListStyle(doc, ListType.Bulleted);
+            listLoaiDieu123.Name = "listLoaiDieu123";
+            listLoaiDieu123.Levels[0].ParagraphFormat.AfterSpacing = 3.2f;
+            listLoaiDieu123.Levels[0].ParagraphFormat.BeforeSpacing = 3.2f;
+            doc.ListStyles.Add(listLoaiDieu123);
+
+            foreach (var type in list123)
+            {
+                Paragraph paraBulletTitle = section.AddParagraph();
+                paraBulletTitle.Format.BeforeSpacing = 3.2F;
+                paraBulletTitle.Format.AfterSpacing = 3.2F;
+                TextRange trBulletTitle = paraBulletTitle.AppendText(type.Name);
+                trBulletTitle.CharacterFormat.Bold = true;
+
+                if (model.QuyetDinhApDungHoaDonDieu1s.Any(x => x.LoaiDieu1 == (LoaiDieu1)type.Value))
+                {
+                    var list = model.QuyetDinhApDungHoaDonDieu1s
+                        .Where(x => x.LoaiDieu1 == (LoaiDieu1)type.Value)
+                        .ToList();
+
+                    foreach (QuyetDinhApDungHoaDonDieu1ViewModel item in list)
+                    {
+                        Paragraph para = section.AddParagraph();
+                        para.AppendText($"{item.Ten} {item.GiaTri}");
+                        para.ListFormat.ApplyStyle(listLoaiDieu123.Name);
+                    }
+                }
+            }
+
+            if (model.QuyetDinhApDungHoaDonDieu1s.Any(x => x.LoaiDieu1 == LoaiDieu1.ThietBi))
+            {
+                var list = model.QuyetDinhApDungHoaDonDieu1s
+                    .Where(x => x.LoaiDieu1 == LoaiDieu1.ThietBi)
+                    .ToList();
+
+                foreach (QuyetDinhApDungHoaDonDieu1ViewModel item in list)
+                {
+                    Paragraph paraThietBi = section.AddParagraph();
+                    paraThietBi.AppendText(item.Ten + ":");
+                    paraThietBi.Format.BeforeSpacing = 3.2F;
+                    paraThietBi.Format.AfterSpacing = 3.2F;
+
+                    Paragraph paraThongTinThietBi = section.AddParagraph();
+                    paraThongTinThietBi.AppendText(item.GiaTri);
+                    paraThongTinThietBi.ListFormat.ApplyStyle(listLoaiDieu123.Name);
+                }
+            }
+
+            Paragraph paraPhanMemUngDung = section.AddParagraph();
+            paraPhanMemUngDung.AppendText("2. Phần mềm ứng dung:");
+            paraPhanMemUngDung.Format.BeforeSpacing = 6;
+            paraPhanMemUngDung.Format.AfterSpacing = 6;
+
+            ListStyle listPhanMemUngDung = new ListStyle(doc, ListType.Bulleted);
+            listPhanMemUngDung.Name = "listPhanMemUngDung";
+            listPhanMemUngDung.Levels[0].ParagraphFormat.AfterSpacing = 3.2f;
+            listPhanMemUngDung.Levels[0].ParagraphFormat.BeforeSpacing = 3.2f;
+            listPhanMemUngDung.Levels[0].NumberPosition = -40;
+            doc.ListStyles.Add(listPhanMemUngDung);
+
+            if (model.QuyetDinhApDungHoaDonDieu1s.Any(x => x.LoaiDieu1 == LoaiDieu1.PhanMemUngDung))
+            {
+                var list = model.QuyetDinhApDungHoaDonDieu1s
+                    .Where(x => x.LoaiDieu1 == LoaiDieu1.PhanMemUngDung)
+                    .ToList();
+
+                foreach (QuyetDinhApDungHoaDonDieu1ViewModel item in list)
+                {
+                    Paragraph para = section.AddParagraph();
+                    para.AppendText($"{item.Ten} {item.GiaTri}");
+                    para.ListFormat.ApplyStyle(listPhanMemUngDung.Name);
+                }
+            }
+
+            Paragraph para3 = section.AddParagraph();
+            para3.AppendText($"3. Bộ phận kỹ thuật hoặc tên nhà cung ứng dịch vụ chịu trách nhiệm về mặt kỹ thuật hoá đơn điện tử, phần mềm ứng dụng: {hoSoHDDTVM.TenDonVi}");
+            para3.Format.BeforeSpacing = 6;
+            para3.Format.AfterSpacing = 6;
+            #endregion
+
+            #region Điều 2
+            Paragraph paraDieu2 = section.AddParagraph();
+            paraDieu2.Format.AfterSpacing = 6;
+            TextRange trDieu2 = paraDieu2.AppendText($"Điều 2. {model.Dieu2}");
+            trDieu2.CharacterFormat.Bold = true;
+
+            Table table = section.AddTable(true);
+
+            string[] Header = { "STT", "Tên hóa đơn", "Mẫu hóa đơn", "Ký hiệu", "Mục đích sử dụng" };
+            string[][] data = { };
+            foreach (var item in model.QuyetDinhApDungHoaDonDieu2s)
+            {
+
+            }
+            #endregion
+
+            FileFormat fileFormat;
+            string fileName;
+
+            if (dinhDangTepMau == DinhDangTepMau.PDF)
+            {
+                fileName = $"{Guid.NewGuid()}.pdf";
+                fileFormat = FileFormat.PDF;
+            }
+            else if (dinhDangTepMau == DinhDangTepMau.DOC)
+            {
+                fileName = $"{Guid.NewGuid()}.doc";
+                fileFormat = FileFormat.Doc;
+            }
+            else
+            {
+                fileName = $"{Guid.NewGuid()}.docx";
+                fileFormat = FileFormat.Docx;
+            }
+
+            string filePath = Path.Combine(destPath, fileName);
+            doc.SaveToFile(filePath, fileFormat);
+
+            byte[] fileByte = File.ReadAllBytes(filePath);
+            File.Delete(filePath);
+
+            return new FileReturn
+            {
+                Bytes = fileByte,
+                ContentType = MimeTypes.GetMimeType(filePath),
+                FileName = Path.GetFileName(filePath)
+            };
         }
 
         public async Task<PagedList<QuyetDinhApDungHoaDonViewModel>> GetAllPagingAsync(QuyetDinhApDungHoaDonParams @params)
@@ -196,6 +406,7 @@ namespace Services.Repositories.Implimentations.DanhMuc
                                                            })
                                                            .ToList(),
                             QuyetDinhApDungHoaDonDieu2s = (from qdd2 in _db.QuyetDinhApDungHoaDonDieu2s
+                                                           join mhd in _db.MauHoaDons on qdd2.MauHoaDonId equals mhd.MauHoaDonId
                                                            where qdd2.QuyetDinhApDungHoaDonId == qd.QuyetDinhApDungHoaDonId
                                                            orderby qdd2.CreatedDate
                                                            select new QuyetDinhApDungHoaDonDieu2ViewModel
@@ -203,6 +414,9 @@ namespace Services.Repositories.Implimentations.DanhMuc
                                                                QuyetDinhApDungHoaDonDieu2Id = qdd2.QuyetDinhApDungHoaDonDieu2Id,
                                                                QuyetDinhApDungHoaDonId = qdd2.QuyetDinhApDungHoaDonId,
                                                                MauHoaDonId = qdd2.MauHoaDonId,
+                                                               TenLoaiHoaDon = mhd.LoaiHoaDon.GetDescription(),
+                                                               MauSo = mhd.MauSo,
+                                                               KyHieu = mhd.KyHieu,
                                                                MucDichSuDung = qdd2.MucDichSuDung,
                                                            })
                                                            .ToList(),
