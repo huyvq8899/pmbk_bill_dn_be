b974b83aa9451c362da5297419547269e273251b
AnhBH <jangkyavn@gmail.com>
2021-08-17 04:38:26 +0700

    tuychonchitiet pending

---
+++ DLL/Enums/KieuDuLieuThietLapTuyChon.cs
@@ -0,0 +1,11 @@
+﻿namespace DLL.Enums
+{
+    public enum KieuDuLieuThietLapTuyChon
+    {
+        Chu = 1,
+        TienTe = 2,
+        SoLuong = 3,
+        Ngay = 4,
+        PhanTram = 5
+    }
+}
---
+++ DLL/Enums/LoaiTuyChinhChiTiet.cs
@@ -0,0 +1,16 @@
+﻿namespace DLL.Enums
+{
+    public enum LoaiTuyChinhChiTiet
+    {
+        ThongTinNguoiBan = 1,
+        ThongTinHoaDon = 2,
+        MauSoKyHieuSoHoaDon = 3,
+        ThongTinNguoiMua = 4,
+        MaQRCode = 5,
+        ThongTinVeHangHoaDichVu = 6,
+        ThongTinVeTongGiaTriHHDV = 7,
+        ThongTinNgoaiTe = 8,
+        ThongTinNguoiKy = 9,
+        ThongTinTraCuu = 10
+    }
+}
