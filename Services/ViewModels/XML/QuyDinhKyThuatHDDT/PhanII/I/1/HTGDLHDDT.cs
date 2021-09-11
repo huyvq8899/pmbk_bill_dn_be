dd09ec6dab76b8e72bdf08f286a1b7ef256bd169
AnhBH <jangkyavn@gmail.com>
2021-09-11 08:51:53 +0700

    update

--- Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/I/1/HTGDLHDDT.cs
+++ Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/I/1/HTGDLHDDT.cs
@@ -1,4 +1,5 @@
-﻿using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Enums;
+﻿using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Attributes;
+using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.I._1
@@ -6,15 +7,27 @@ namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.I._1
     public partial class HTGDLHDDT
     {
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "NNT địa bàn khó khăn (Doanh nghiệp nhỏ và vừa, hợp tác xã, hộ, cá nhân kinh doanh tại địa bàn có điều kiện kinh tế xã hội khó khăn, địa bàn có điều kiện kinh tế xã hội đặc biệt khó khăn)")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung NNTDBKKhan { get; set; }
 
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "NNT khác theo đề nghị UBND (Doanh nghiệp nhỏ và vừa khác theo đề nghị của Ủy ban nhân dân tỉnh, thành phố trực thuộc trung ương gửi Bộ Tài chính trừ doanh nghiệp hoạt động tại các khu kinh tế, khu công nghiệp, khu công nghệ cao)")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung NNTKTDNUBND { get; set; }
 
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "Chuyển dữ liệu trực tiếp đến CQT (Chuyển dữ liệu hóa đơn điện tử trực tiếp đến cơ quan thuế (điểm b1, khoản 3, Điều 22 của Nghị định))")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung CDLTTDCQT { get; set; }
 
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "Chuyển dữ liệu qua T-VAN (Thông qua tổ chức cung cấp dịch vụ hóa đơn điện tử (điểm b2, khoản 3, Điều 22 của Nghị định))")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung CDLQTVAN { get; set; }
     }
 }
--- Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/I/1/HTHDon.cs
+++ Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/I/1/HTHDon.cs
@@ -1,4 +1,5 @@
-﻿using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Enums;
+﻿using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Attributes;
+using Services.ViewModels.XML.QuyDinhKyThuatHDDT.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.I._1
@@ -6,9 +7,15 @@ namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.I._1
     public partial class HTHDon
     {
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "Có mã (Hình thức hóa đơn có mã của CQT)")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung CMa { get; set; }
 
         [Required]
+        [MaxLength(1)]
+        [Display(Name = "Không có mã (Hình thức hóa đơn không có mã của CQT)")]
+        [CustomDataType(CustomDataType.Number)]
         public ADung KCMa { get; set; }
     }
 }
