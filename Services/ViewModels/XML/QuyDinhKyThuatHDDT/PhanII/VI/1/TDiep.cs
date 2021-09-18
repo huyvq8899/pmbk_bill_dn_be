da8a6725d39590911c09f6e3d0f75a5f0b37beec
manhphan <manhpq@pmbk.vn>
2021-09-18 07:49:33 +0700

    Phan2_Khoan6_Muc1

---
+++ Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/VI/1/TDiep.cs
@@ -0,0 +1,19 @@
+﻿using Services.ViewModels.XML.QuyDinhKyThuatHDDT.LogEntities;
+using Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.a;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.VI._1
+{
+    public partial class TDiep
+    {
+        public TTChungThongDiep TTChung { get; set; }
+        public DLieu DLieu { get; set; }
+    }
+
+    public partial class DLieu
+    {
+        public List<HDon> HDon { get; set; }
+    }
+}
