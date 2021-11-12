a8bab9ae84c644d748d9242da5603b81d2a18dd6
Tran Tang Doan <doantt53@gmail.com>
2021-11-12 07:42:40 +0700

    Update log transfer

--- Services/Helper/TransferLogHelper.cs
+++ Services/Helper/TransferLogHelper.cs
@@ -11,76 +11,13 @@ namespace Services.Helper
 {
     public static class TransferLogHelper
     {
-        //public static async Task<bool> AddTransferLogReceiveAsync(this Datacontext db, ThongDiepPhanHoiParams data)
-        //{
-        //    bool res = false;
-        //    try
-        //    {
-        //        if (data == null)
-        //        {
-        //            return res;
-        //        }
-
-        //        TransferLog log = new TransferLog
-        //        {
-        //            TransferLogId = Guid.NewGuid().ToString(),
-        //            DateTime = DateTime.Now,
-        //            MLTDiep = data.MLTDiep,
-        //            MTDiep = data.MTDiep,
-        //            MTDTChieu = data.MTDTChieu,
-        //            Type = 2,
-        //            XMLData = data.DataXML
-        //        };
-
-        //        // Save database.
-        //        await db.TransferLogs.AddAsync(log);
-        //        await db.SaveChangesAsync();
-
-        //        res = true;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Tracert.WriteLog(string.Empty, ex);
-        //    }
-
-        //    return res;
-        //}
-
-        //public static async Task<bool> AddTransferLogSendAsync(this Datacontext db, ThongDiepPhanHoiParams data)
-        //{
-        //    bool res = false;
-        //    try
-        //    {
-        //        if (data == null)
-        //        {
-        //            return res;
-        //        }
-
-        //        TransferLog log = new TransferLog
-        //        {
-        //            TransferLogId = Guid.NewGuid().ToString(),
-        //            DateTime = DateTime.Now,
-        //            MLTDiep = data.MLTDiep,
-        //            MTDiep = data.MTDiep,
-        //            MTDTChieu = data.MTDTChieu,
-        //            Type = 1,
-        //            XMLData = data.DataXML
-        //        };
-
-        //        // Save database.
-        //        await db.TransferLogs.AddAsync(log);
-        //        await db.SaveChangesAsync();
-
-        //        res = true;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Tracert.WriteLog(string.Empty, ex);
-        //    }
-
-        //    return res;
-        //}
-
+        /// <summary>
+        /// Add log send & receive from TVAN
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="dataXML"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public static async Task<bool> AddTransferLog(this Datacontext db, string dataXML, int type = 1)
         {
             bool res = false;
