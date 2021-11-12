900fdac8a25a0c2d967c7cea2e7ac286b9f6da03
CanhNV <namnguyenxuan987@gmail.com>
2021-11-12 16:58:36 +0700

    Cập nhật 121121

---
+++ Services/ViewModels/XML/ThongDiepGuiNhanCQT/TDiepNhan999/TDiep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.ViewModels.XML.ThongDiepGuiNhanCQT.TDiepNhan999
+{
+    public partial class TDiep
+    {
+        public TTChung TTChung { set; get; }
+        public DLieu DLieu { set; get; }
+    }
+}
