d40ec582aaea91aaf3f71eb9b3ee44f9c4cb69e8
thunh1710 <thunth.1710@gmail.com>
2021-11-18 11:00:38 +0700

    merge and push sua a Kien

--- Services/ViewModels/XML/QuyDinhKyThuatHDDT/Enums/TTXNCQT.cs
+++ Services/ViewModels/XML/QuyDinhKyThuatHDDT/Enums/TTXNCQT.cs
@@ -7,10 +7,10 @@ namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.Enums
     [Serializable]
     public enum TTXNCQT
     {
-        [Description("Trường hợp chấp nhận đăng ký/thay đổi thông tin sử dụng hóa đơn điện tử")]
+        [Description("Chấp nhận cho phép NNT sử dụng hóa đơn điện tử.")]
         [XmlEnum("1")]
         ChapNhan = 1,
-        [Description("Trường hợp không chấp nhận đăng ký/thay đổi thông tin sử dụng hóa đơn điện tử")]
+        [Description("Không chấp nhận cho phép NNT sử dụng hóa đơn điện tử.")]
         [XmlEnum("2")]
         KhongChapNhan = 2
     }
