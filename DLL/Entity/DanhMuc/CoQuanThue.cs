8cb8f21bf95b5391dccc5b4bce197cc2f73b12a1
thunh1710 <thunth.1710@gmail.com>
2021-11-19 17:41:26 +0700

    push sua

---
+++ DLL/Entity/DanhMuc/CoQuanThue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.Entity.DanhMuc
+{
+    public class CoQuanThue : ICloneable
+    {
+        public string Id { get; set; }
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public string DiaChi { get; set; }
+        public string MaCQTCapCuc { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public List<CoQuanThue> InitData()
+        {
+            List<CoQuanThue> datas = new List<CoQuanThue>()
+            {
+                new CoQuanThue
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Ma = "10100",
+                    Ten = ""
+                }
+            };
+
+            return datas;
+        }
+    }
+}
---
+++ Services/ViewModels/DanhMuc/CoQuanThueViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.ViewModels.DanhMuc
+{
+    public class CoQuanThueViewModel
+    {
+        public string Id { get; set; }
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public string DiaChi { get; set; }
+        public string MaCQTCapCuc { get; set; }
+    }
+}
