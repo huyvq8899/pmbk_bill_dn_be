0be5390a9e374c1a7c7d05a5ff060646d0c84a81
AnhBH <jangkyavn@gmail.com>
2021-11-20 07:43:43 +0700

    add migrate

---
+++ DLL/Migrations/20211120004303_dieu-chinh-col-thiet-lap-truong-du-lieu.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class dieuchinhcolthietlaptruongdulieu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ThietLapTruongDuLieus_MauHoaDons_MauHoaDonId",
+                table: "ThietLapTruongDuLieus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ThietLapTruongDuLieus_MauHoaDonId",
+                table: "ThietLapTruongDuLieus");
+
+            migrationBuilder.RenameColumn(
+                name: "MauHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                newName: "GiaTri");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GiaTri",
+                table: "ThietLapTruongDuLieus",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThietLapTruongDuLieus_BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                column: "BoKyHieuHoaDonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ThietLapTruongDuLieus_BoKyHieuHoaDons_BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                column: "BoKyHieuHoaDonId",
+                principalTable: "BoKyHieuHoaDons",
+                principalColumn: "BoKyHieuHoaDonId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ThietLapTruongDuLieus_BoKyHieuHoaDons_BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ThietLapTruongDuLieus_BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus");
+
+            migrationBuilder.DropColumn(
+                name: "BoKyHieuHoaDonId",
+                table: "ThietLapTruongDuLieus");
+
+            migrationBuilder.RenameColumn(
+                name: "GiaTri",
+                table: "ThietLapTruongDuLieus",
+                newName: "MauHoaDonId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MauHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ThietLapTruongDuLieus_MauHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                column: "MauHoaDonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ThietLapTruongDuLieus_MauHoaDons_MauHoaDonId",
+                table: "ThietLapTruongDuLieus",
+                column: "MauHoaDonId",
+                principalTable: "MauHoaDons",
+                principalColumn: "MauHoaDonId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
