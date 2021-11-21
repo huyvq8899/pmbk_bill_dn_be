81715053e78af3805553e43a645f1081295d38a3
CanhNV <namnguyenxuan987@gmail.com>
2021-11-21 09:37:39 +0700

    Cập nhật 211121

--- Services/Helper/Params/HoaDon/ParamsSendMail.cs
+++ Services/Helper/Params/HoaDon/ParamsSendMail.cs
@@ -17,4 +17,16 @@ namespace Services.Helper.Params.HoaDon
         public string Link { get; set; }
         public string BienBanDieuChinhId { get; set; }
     }
+
+    public class ParamsSendMailThongTinHoaDon
+    {
+        public string ThongTinHoaDonId { get; set; }
+        public string ToMail { get; set; }
+        public string TenNguoiNhan { get; set; }
+        public string CC { get; set; }
+        public string BCC { get; set; }
+        public string SoDienThoaiNguoiNhan { get; set; }
+        public int? LoaiEmail { get; set; }
+        public string Link { get; set; }
+    }
 }
