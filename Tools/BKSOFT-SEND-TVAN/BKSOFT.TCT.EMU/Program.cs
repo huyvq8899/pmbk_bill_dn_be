7d1e7d84fba8cee23a7f2c461d12f7d93d424a35
Tran Tang Doan <doantt53@gmail.com>
2021-11-22 14:15:47 +0700

    Update for TCT

--- Tools/BKSOFT-SEND-TVAN/BKSOFT.TCT.EMU/Program.cs
+++ Tools/BKSOFT-SEND-TVAN/BKSOFT.TCT.EMU/Program.cs
@@ -51,69 +51,69 @@ namespace BKSOFT.TCT.EMU
 
 
 
-            try
-            {
+            //try
+            //{
 
 
-                TVanInfo info = new TVanInfo();
+            //    TVanInfo info = new TVanInfo();
 
-                // Queue 
-                info.QueHostName = ConfigurationManager.AppSettings["TVAN_HostName"];
-                info.QueUserName = ConfigurationManager.AppSettings["Que_UserName"];
-                info.QuePassword = "ZjM&1O6ds2EQ";
-                info.QuePort = Convert.ToInt32(ConfigurationManager.AppSettings["TVAN_Port"]);
-                info.QueName = ConfigurationManager.AppSettings["TVAN_QueueName"];
+            //    // Queue 
+            //    info.QueHostName = ConfigurationManager.AppSettings["TVAN_HostName"];
+            //    info.QueUserName = ConfigurationManager.AppSettings["Que_UserName"];
+            //    info.QuePassword = "ZjM&1O6ds2EQ";
+            //    info.QuePort = Convert.ToInt32(ConfigurationManager.AppSettings["TVAN_Port"]);
+            //    info.QueName = ConfigurationManager.AppSettings["TVAN_QueueName"];
 
-                info.ApiUrl = ConfigurationManager.AppSettings["TVAN_Url"];
-                info.ApiTaxCode = ConfigurationManager.AppSettings["TVAN_TaxCode"];
-                info.ApiUserName = ConfigurationManager.AppSettings["TVAN_UserName"];
-                info.ApiPassword = ConfigurationManager.AppSettings["TVAN_PassWord"];
+            //    info.ApiUrl = ConfigurationManager.AppSettings["TVAN_Url"];
+            //    info.ApiTaxCode = ConfigurationManager.AppSettings["TVAN_TaxCode"];
+            //    info.ApiUserName = ConfigurationManager.AppSettings["TVAN_UserName"];
+            //    info.ApiPassword = ConfigurationManager.AppSettings["TVAN_PassWord"];
 
-                //// Open queue
-                //ConnectionFactory factory = new ConnectionFactory();
-                //factory.UserName = "E65566E0BD464B0890038BAC43C2373B";
-                //factory.Password = "7^NM6vzxBGFV";
-                //factory.VirtualHost = "/";
-                ////factory.Protocol = Protocols.FromEnvironment();
-                //factory.HostName = "mq.softdreams.vn";
-                //factory.Port = info.QuePort;
-                //IConnection conn = factory.CreateConnection();
+            //    //// Open queue
+            //    //ConnectionFactory factory = new ConnectionFactory();
+            //    //factory.UserName = "E65566E0BD464B0890038BAC43C2373B";
+            //    //factory.Password = "7^NM6vzxBGFV";
+            //    //factory.VirtualHost = "/";
+            //    ////factory.Protocol = Protocols.FromEnvironment();
+            //    //factory.HostName = "mq.softdreams.vn";
+            //    //factory.Port = info.QuePort;
+            //    //IConnection conn = factory.CreateConnection();
 
-                var factory = new ConnectionFactory()
-                {
-                    HostName = info.QueHostName,
-                    Port = info.QuePort,
-                    UserName = info.QueUserName,
-                    Password = info.QuePassword,
-                    VirtualHost = "/",
-                    AutomaticRecoveryEnabled = true,
-                    RequestedHeartbeat = 60,
-                    ContinuationTimeout = new TimeSpan(0, 0, 5)        // 5 second
-                };
-                var connection = factory.CreateConnection();
-                var channel = connection.CreateModel();
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += Consumer_Received;
-                // this consumer tag identifies the subscription
-                // when it has to be cancelled
-                string consumerTag = channel.BasicConsume(info.QueName, false, consumer);
+            //    var factory = new ConnectionFactory()
+            //    {
+            //        HostName = info.QueHostName,
+            //        Port = info.QuePort,
+            //        UserName = info.QueUserName,
+            //        Password = info.QuePassword,
+            //        VirtualHost = "/",
+            //        AutomaticRecoveryEnabled = true,
+            //        RequestedHeartbeat = 60,
+            //        ContinuationTimeout = new TimeSpan(0, 0, 5)        // 5 second
+            //    };
+            //    var connection = factory.CreateConnection();
+            //    var channel = connection.CreateModel();
+            //    var consumer = new EventingBasicConsumer(channel);
+            //    consumer.Received += Consumer_Received;
+            //    // this consumer tag identifies the subscription
+            //    // when it has to be cancelled
+            //    string consumerTag = channel.BasicConsume(info.QueName, false, consumer);
 
-                //// Send hoa don cap ma
-                //XmlDocument xmlDoc = new XmlDocument();
-                //xmlDoc.Load(@"D:\XML\TD-0e37dc3b-134c-40d3-89f7-490981029e5d.xml");
+            //    //// Send hoa don cap ma
+            //    //XmlDocument xmlDoc = new XmlDocument();
+            //    //xmlDoc.Load(@"D:\XML\TD-0e37dc3b-134c-40d3-89f7-490981029e5d.xml");
 
-                //// Send Tvan
-                //TVANHelper tvan = new TVANHelper(info);
-                //tvan.TVANSendData("api/invoice/send", xmlDoc.OuterXml);
+            //    //// Send Tvan
+            //    //TVANHelper tvan = new TVANHelper(info);
+            //    //tvan.TVANSendData("api/invoice/send", xmlDoc.OuterXml);
 
-                Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
+            //    Console.ReadKey();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
 
-                FileLog.WriteLog(string.Empty, ex);
-            }
+            //    FileLog.WriteLog(string.Empty, ex);
+            //}
         }
 
         private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
