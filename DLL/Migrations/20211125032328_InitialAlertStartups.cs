3d50aec8795b5bb949f09a10ed18454b1d6c4ff4
Van Lung <vlung05@gmail.com>
2021-11-26 15:25:42 +0700

    add thông báo update csk

---
+++ DLL/Migrations/20211125032328_InitialAlertStartups.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class InitialAlertStartups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlertStartups",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    SubTitle = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifyDate = table.Column<DateTime>(nullable: true),
+                    ModifyBy = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlertStartups", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlertStartups");
+        }
+    }
+}
