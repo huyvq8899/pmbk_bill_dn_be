fa5e448080768c4abd1bba6ea9cfea50591be80d
thunh1710 <thunth.1710@gmail.com>
2021-12-02 07:41:54 +0700

    push all code

--- Services/Repositories/Interfaces/DanhMuc/IHoSoHDDTService.cs
+++ Services/Repositories/Interfaces/DanhMuc/IHoSoHDDTService.cs
@@ -1,5 +1,6 @@
 ﻿using ManagementServices.Helper;
 using Services.ViewModels.DanhMuc;
+using Services.ViewModels.QuyDinhKyThuat;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,11 +9,11 @@ namespace Services.Repositories.Interfaces.DanhMuc
     public interface IHoSoHDDTService
     {
         Task<HoSoHDDTViewModel> GetDetailAsync();
-
         Task<HoSoHDDTViewModel> InsertAsync(HoSoHDDTViewModel model);
         Task<bool> UpdateAsync(HoSoHDDTViewModel model);
         List<CityParam> GetListCoQuanThueCapCuc();
         List<DistrictsParam> GetListCoQuanThueQuanLy();
         List<CityParam> GetListCity();
+        Task<List<ChungThuSoSuDungViewModel>> GetDanhSachChungThuSoSuDung();
     }
 }
