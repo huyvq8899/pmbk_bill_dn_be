d48397c23be1289078ab814959241b37476f4dac
AnhBH <jangkyavn@gmail.com>
2021-12-03 14:42:59 +0700

    update xml hoa don

--- Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/II/5-6/TDiep.cs
+++ Services/ViewModels/XML/QuyDinhKyThuatHDDT/PhanII/II/5-6/TDiep.cs
@@ -1,9 +1,11 @@
 ﻿
 using Services.ViewModels.XML.QuyDinhKyThuatHDDT.LogEntities;
-using Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.a;
+using HDonGTGT = Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.a.HDon;
+using HDonBH = Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.b.HDon;
 
 namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._5_6
 {
+    //------------------ Hóa đơn GTGT ----------------------
     public partial class TDiep
     {
         public TTChungThongDiep TTChung { get; set; }
@@ -12,6 +14,18 @@ namespace Services.ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._5_6
 
     public partial class DLieu
     {
-        public HDon HDon { get; set; }
+        public HDonGTGT HDon { get; set; }
+    }
+
+    //------------------ Hóa đơn bán hàng ----------------------
+    public partial class TDiep2
+    {
+        public TTChungThongDiep TTChung { get; set; }
+        public DLieu2 DLieu { get; set; }
+    }
+
+    public partial class DLieu2
+    {
+        public HDonBH HDon { get; set; }
     }
 }
