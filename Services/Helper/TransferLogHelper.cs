a703f44f6120ba73c2a6546b5fa2929d1732931a
Tran Tang Doan <doantt53@gmail.com>
2021-12-04 14:51:28 +0700

    Update to log

--- Services/Helper/TransferLogHelper.cs
+++ Services/Helper/TransferLogHelper.cs
@@ -2,13 +2,19 @@
 using DLL.Entity;
 using Microsoft.AspNetCore.Http;
 using Services.Helper.XmlModel;
+using Services.ViewModels.XML.QuyDinhKyThuatHDDT.LogEntities;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace Services.Helper
 {
@@ -31,43 +37,48 @@ namespace Services.Helper
                     return res;
                 }
 
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(dataXML);
+                TTChungThongDiep info = new TTChungThongDiep();
+
+                // Get Thông tin chung
+                byte[] bytes = Encoding.UTF8.GetBytes(dataXML);
+                using (MemoryStream ms = new MemoryStream(bytes))
+                {
+                    using (StreamReader reader = new StreamReader(ms))
+                    {
+                        XDocument xDoc = XDocument.Load(reader);
+                        info = xDoc.Descendants("TTChung")
+                                       .Select(x => new TTChungThongDiep
+                                       {
+                                           PBan = x.Element(nameof(info.PBan)).Value,
+                                           MNGui = x.Element(nameof(info.MNGui)).Value,
+                                           MNNhan = x.Element(nameof(info.MNNhan)).Value,
+                                           MLTDiep = x.Element(nameof(info.MLTDiep)).Value,
+                                           MTDiep = x.Element(nameof(info.MTDiep)).Value,
+                                           MTDTChieu = x.Element(nameof(info.MTDTChieu)).Value,
+                                           MST = x.Element(nameof(info.MST)).Value
+                                       }).FirstOrDefault();
+                    }
+                }
 
                 TransferLog log = new TransferLog
                 {
                     TransferLogId = Guid.NewGuid().ToString(),
                     DateTime = DateTime.Now,
                     Type = type,
+                    MNGui = info.MNGui,
+                    MNNhan = info.MNNhan,
+                    MLTDiep = Convert.ToInt32(info.MLTDiep),
+                    MTDiep = info.MTDiep,
+                    MTDTChieu = info.MTDTChieu,
                     XMLData = dataXML
                 };
-
-                // MNGui
-                XmlNodeList elemList = doc.GetElementsByTagName("MNGui");
-                log.MNGui = elemList?[0].InnerXml;
-
-                // MNNhan
-                elemList = doc.GetElementsByTagName("MNNhan");
-                log.MNNhan = elemList?[0].InnerXml;
-
-                // MLTDiep
-                elemList = doc.GetElementsByTagName("MLTDiep");
-                if (elemList != null)
-                {
-                    log.MLTDiep = Convert.ToInt32(elemList?[0].InnerXml);
-                }
-
-                // MTDiep
-                elemList = doc.GetElementsByTagName("MTDiep");
-                log.MTDiep = elemList?[0].InnerXml;
-
-                // MTDTChieu
-                elemList = doc.GetElementsByTagName("MTDTChieu");
-                log.MTDTChieu = elemList?[0].InnerXml;
 
                 // Save database.
                 await db.TransferLogs.AddAsync(log);
                 await db.SaveChangesAsync();
+
+                // Send to log total
+                SendViaSocketConvert("127.0.0.1", 40000, DataHelper.Base64Decode(dataXML));
 
                 res = true;
             }
@@ -79,45 +90,145 @@ namespace Services.Helper
             return res;
         }
 
-        //public static async Task<bool> AddToTCTTransfer(TransferLog log)
+        private static string SendViaSocketConvert(string ip, int port, string msg)
+        {
+            string recString = string.Empty;
+
+            try
+            {
+                // Data buffer for incoming data.  
+                byte[] bytes = new byte[1024];
+
+                // Connect to a remote device.  
+                try
+                {
+                    IPAddress ipAddress = IPAddress.Parse(ip);
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                    // Create a TCP/IP  socket.  
+                    Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    // Connect the socket to the remote endpoint. Catch any errors.  
+                    try
+                    {
+                        sender.Connect(remoteEP);
+
+                        // Send the data through the socket.  
+                        int bytesSent = sender.Send(Encoding.ASCII.GetBytes(msg));
+
+                        // Receive the response from the remote device.  
+                        int bytesRec = sender.Receive(bytes);
+                        recString = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        // Release the socket.  
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        Tracert.WriteLog(string.Empty, ane);
+                    }
+                    catch (SocketException se)
+                    {
+                        Tracert.WriteLog(string.Empty, se);
+                    }
+                    catch (Exception e)
+                    {
+                        Tracert.WriteLog(string.Empty, e);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Tracert.WriteLog(string.Empty, e);
+                }
+            }
+            catch (Exception e)
+            {
+                Tracert.WriteLog(string.Empty, e);
+            }
+
+            return recString;
+        }
+
+        //public static async Task<bool> AddToTCTTransfer(string dataXML)
         //{
-        //    string con = "";
-        //    using (SqlConnection connection = new SqlConnection(con))
+        //    bool res = false;
+
+        //    try
         //    {
-        //        using (SqlCommand command = new SqlCommand("usp_InsertMessage", connection))
+        //        if (string.IsNullOrEmpty(dataXML))
         //        {
-        //            command.CommandType = CommandType.StoredProcedure;
-
-        //            command.Parameters.Add("@DateTime", SqlDbType.DateTime);
-        //            command.Parameters["@DateTime"].Value = DateTime.Now;
-
-        //            command.Parameters.Add("@MNGui", SqlDbType.NVarChar);
-        //            command.Parameters["@MNGui"].Value = log.MNGui;
-
-        //            command.Parameters.Add("@MNNhan", SqlDbType.NVarChar);
-        //            command.Parameters["@MNNhan"].Value = log.MNNhan;
-
-        //            command.Parameters.Add("@MLTDiep", SqlDbType.Int);
-        //            command.Parameters["@MLTDiep"].Value = log.MLTDiep;
-
-        //            command.Parameters.Add("@MTDiep", SqlDbType.NVarChar);
-        //            command.Parameters["@MTDiep"].Value = log.MTDiep;
-
-        //            command.Parameters.Add("@MTDTChieu", SqlDbType.NVarChar);
-        //            command.Parameters["@MTDTChieu"].Value = log.MTDTChieu;
-
-        //            command.Parameters.Add("@MST", SqlDbType.NVarChar);
-        //            command.Parameters["@MST"].Value = log.MST;
-
-        //            command.Parameters.Add("@DataXML", SqlDbType.NVarChar);
-        //            command.Parameters["@DataXML"].Value = log.XMLData;
-
-        //            await connection.OpenAsync();
-        //            SqlDataReader reader = await command.ExecuteReaderAsync();
+        //            return res;
         //        }
+
+        //        string con = "Server=10.10.20.11;Database=TCTTranfer;User Id=sa;Password=Bk9108vn;MultipleActiveResultSets=true";
+
+        //        TTChungThongDiep log = new TTChungThongDiep();
+
+        //        // Get Thông tin chung
+        //        byte[] bytes = Encoding.UTF8.GetBytes(dataXML);
+        //        using (MemoryStream ms = new MemoryStream(bytes))
+        //        {
+        //            using (StreamReader reader = new StreamReader(ms))
+        //            {
+        //                XDocument xDoc = XDocument.Load(reader);
+        //                log = xDoc.Descendants("TTChung")
+        //                               .Select(x => new TTChungThongDiep
+        //                               {
+        //                                   PBan = x.Element(nameof(log.PBan)).Value,
+        //                                   MNGui = x.Element(nameof(log.MNGui)).Value,
+        //                                   MNNhan = x.Element(nameof(log.MNNhan)).Value,
+        //                                   MLTDiep = x.Element(nameof(log.MLTDiep)).Value,
+        //                                   MTDiep = x.Element(nameof(log.MTDiep)).Value,
+        //                                   MTDTChieu = x.Element(nameof(log.MTDTChieu)).Value,
+        //                                   MST = x.Element(nameof(log.MST)).Value
+        //                               }).FirstOrDefault();
+        //            }
+        //        }
+
+        //        using (SqlConnection connection = new SqlConnection(con))
+        //        {
+        //            using (SqlCommand command = new SqlCommand("usp_InsertMessage", connection))
+        //            {
+        //                command.CommandType = CommandType.StoredProcedure;
+
+        //                command.Parameters.Add("@DateTime", SqlDbType.DateTime);
+        //                command.Parameters["@DateTime"].Value = DateTime.Now;
+
+        //                command.Parameters.Add("@MNGui", SqlDbType.NVarChar);
+        //                command.Parameters["@MNGui"].Value = log.MNGui;
+
+        //                command.Parameters.Add("@MNNhan", SqlDbType.NVarChar);
+        //                command.Parameters["@MNNhan"].Value = log.MNNhan;
+
+        //                command.Parameters.Add("@MLTDiep", SqlDbType.Int);
+        //                command.Parameters["@MLTDiep"].Value = log.MLTDiep;
+
+        //                command.Parameters.Add("@MTDiep", SqlDbType.NVarChar);
+        //                command.Parameters["@MTDiep"].Value = log.MTDiep;
+
+        //                command.Parameters.Add("@MTDTChieu", SqlDbType.NVarChar);
+        //                command.Parameters["@MTDTChieu"].Value = log.MTDTChieu;
+
+        //                command.Parameters.Add("@MST", SqlDbType.NVarChar);
+        //                command.Parameters["@MST"].Value = log.MST;
+
+        //                command.Parameters.Add("@DataXML", SqlDbType.NVarChar);
+        //                command.Parameters["@DataXML"].Value = dataXML;
+
+        //                await connection.OpenAsync();
+        //                SqlDataReader reader = await command.ExecuteReaderAsync();
+        //            }
+        //        }
+
+        //        res = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Tracert.WriteLog(string.Empty, ex);
         //    }
 
-        //    return true;
+        //    return res;
         //}
     }
 }
--- Tools/BKSOFT-TVAN/BKSOFT.TCT/DAL/TCTTranfer.Context.cs
+++ Tools/BKSOFT-TVAN/BKSOFT.TCT/DAL/TCTTranfer.Context.cs
@@ -12,6 +12,8 @@ namespace BKSOFT.TCT.DAL
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
     public partial class TCTTranferEntities : DbContext
     {
@@ -26,5 +28,46 @@ namespace BKSOFT.TCT.DAL
         }
     
         public virtual DbSet<TIVan> TIVans { get; set; }
+    
+        public virtual int usp_InsertMessage(Nullable<System.DateTime> dateTime, string mNGui, string mNNhan, Nullable<int> mLTDiep, string mTDiep, string mTDTChieu, string mST, string dataXML, Nullable<bool> status)
+        {
+            var dateTimeParameter = dateTime.HasValue ?
+                new ObjectParameter("DateTime", dateTime) :
+                new ObjectParameter("DateTime", typeof(System.DateTime));
+    
+            var mNGuiParameter = mNGui != null ?
+                new ObjectParameter("MNGui", mNGui) :
+                new ObjectParameter("MNGui", typeof(string));
+    
+            var mNNhanParameter = mNNhan != null ?
+                new ObjectParameter("MNNhan", mNNhan) :
+                new ObjectParameter("MNNhan", typeof(string));
+    
+            var mLTDiepParameter = mLTDiep.HasValue ?
+                new ObjectParameter("MLTDiep", mLTDiep) :
+                new ObjectParameter("MLTDiep", typeof(int));
+    
+            var mTDiepParameter = mTDiep != null ?
+                new ObjectParameter("MTDiep", mTDiep) :
+                new ObjectParameter("MTDiep", typeof(string));
+    
+            var mTDTChieuParameter = mTDTChieu != null ?
+                new ObjectParameter("MTDTChieu", mTDTChieu) :
+                new ObjectParameter("MTDTChieu", typeof(string));
+    
+            var mSTParameter = mST != null ?
+                new ObjectParameter("MST", mST) :
+                new ObjectParameter("MST", typeof(string));
+    
+            var dataXMLParameter = dataXML != null ?
+                new ObjectParameter("DataXML", dataXML) :
+                new ObjectParameter("DataXML", typeof(string));
+    
+            var statusParameter = status.HasValue ?
+                new ObjectParameter("Status", status) :
+                new ObjectParameter("Status", typeof(bool));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_InsertMessage", dateTimeParameter, mNGuiParameter, mNNhanParameter, mLTDiepParameter, mTDiepParameter, mTDTChieuParameter, mSTParameter, dataXMLParameter, statusParameter);
+        }
     }
 }
