c8398f9661c34001104d586236b2f969b935f622
AnhBH <jangkyavn@gmail.com>
2021-12-04 08:54:24 +0700

    add migrate

--- DLL/Migrations/20211204012329_add-loai-chiet-khau.Designer.cs
+++ DLL/Migrations/20211204012329_add-loai-chiet-khau.Designer.cs
@@ -4,14 +4,16 @@ using DLL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DLL.Migrations
 {
     [DbContext(typeof(Datacontext))]
-    partial class DatacontextModelSnapshot : ModelSnapshot
+    [Migration("20211204012329_add-loai-chiet-khau")]
+    partial class addloaichietkhau
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
