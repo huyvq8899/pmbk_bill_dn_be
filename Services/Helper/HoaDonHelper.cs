93901eb27386fc908aafb67e7dc1a29f4df8006b
CanhNV <namnguyenxuan987@gmail.com>
2021-12-08 15:02:17 +0700

    cập nhật sửa hóa đơn thay thế

---
+++ Services/Helper/HoaDonHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Services.Helper
+{
+    public static class HoaDonHelper
+    {
+        public static string GetDienGiaiTrangThaiHoaDon(int? hinhThucXoaBo)
+        {
+            if (hinhThucXoaBo == (int)DLL.Enums.HinhThucXoabo.HinhThuc3)
+            {
+                return "Đã hủy do hợp đồng hủy";
+            }
+            else if (hinhThucXoaBo == (int)DLL.Enums.HinhThucXoabo.HinhThuc1 ||
+                hinhThucXoaBo == (int)DLL.Enums.HinhThucXoabo.HinhThuc4 ||
+                hinhThucXoaBo == (int)DLL.Enums.HinhThucXoabo.HinhThuc6)
+            {
+                return "Đã hủy do sai sót";
+            }
+            else if (hinhThucXoaBo != null)
+            {
+                return "Đã bị xóa bỏ";
+            }
+
+            return "";
+        }
+    }
+}
