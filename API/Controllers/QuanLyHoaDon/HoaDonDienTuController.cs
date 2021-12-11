696fb2a2f54fba03a81718b852334e6f4c280e78
thunh1710 <thunth.1710@gmail.com>
2021-12-11 13:15:00 +0700

    push all code

--- API/Controllers/QuanLyHoaDon/HoaDonDienTuController.cs
+++ API/Controllers/QuanLyHoaDon/HoaDonDienTuController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using Services.Enums;
 using Services.Helper;
+using Services.Helper.Params;
 using Services.Helper.Params.HoaDon;
 using Services.Repositories.Interfaces;
 using Services.Repositories.Interfaces.QuanLyHoaDon;
@@ -186,9 +187,9 @@ namespace API.Controllers.QuanLyHoaDon
 
         [AllowAnonymous]
         [HttpPost("FindSignatureElement")]
-        public async Task<IActionResult> FindSignatureElement(string filePath)
+        public async Task<IActionResult> FindSignatureElement(CTSParams @params)
         {
-            var result = _traCuuService.FindSignatureElement(filePath);
+            var result = _traCuuService.FindSignatureElement(@params.FilePath);
             return Ok(result);
         }
 
---
+++ Services/Helper/Params/CTSParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Helper.Params
+{
+    public class CTSParams
+    {
+        public string FilePath { get; set; }
+    }
+}
