48eeef7a82545f175c7be38d8a5d5df764747213
AnhBH <jangkyavn@gmail.com>
2021-12-14 11:04:51 +0700

    update

--- API/Controllers/QuanLyHoaDon/HoaDonDienTuController.cs
+++ API/Controllers/QuanLyHoaDon/HoaDonDienTuController.cs
@@ -929,11 +929,13 @@ namespace API.Controllers.QuanLyHoaDon
                 {
                     var result = await _hoaDonDienTuService.InsertImportHoaDonAsync(data);
                     transaction.Commit();
+                    Tracert.WriteLog("logInsertExcelOK: " + result);
                     return Ok(result);
                 }
                 catch (Exception e)
                 {
                     transaction.Rollback();
+                    Tracert.WriteLog("logInsertExcel: ", e);
                     return Ok(false);
                 }
             }
