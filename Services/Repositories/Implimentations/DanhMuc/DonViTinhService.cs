98df3b486fb23b2e417bb364f0d17d77f3994e5d
thunh1710 <thunth.1710@gmail.com>
2021-12-21 14:46:10 +0700

    push all code

--- Services/Repositories/Implimentations/DanhMuc/DonViTinhService.cs
+++ Services/Repositories/Implimentations/DanhMuc/DonViTinhService.cs
@@ -49,7 +49,7 @@ namespace Services.Repositories.Implimentations.DanhMuc
         public async Task<bool> CheckTrungMaAsync(DonViTinhViewModel model)
         {
             bool result = await _db.DonViTinhs
-                .AnyAsync(x => x.Ten.ToUpper().Trim() == model.Ten.ToUpper().Trim());
+                .AnyAsync(x => String.Compare(x.Ten.Trim(), model.Ten.Trim(), false) == 0);
 
             return result;
         }
