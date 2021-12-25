9c11827a4bcbeec5c4a48d54e81de53d4bb921fc
Tran Tang Doan <doantt53@gmail.com>
2021-12-25 16:14:57 +0700

    Update TVAN

--- Tools/BKSOFT-TVAN/BKSOFT.TVAN/ThreadQueueTVAN.cs
+++ Tools/BKSOFT-TVAN/BKSOFT.TVAN/ThreadQueueTVAN.cs
@@ -22,9 +22,7 @@ namespace BKSOFT.TVAN
 
         private static string URI = "amqp://U-0200784873-68B111A604454C12A6F616CF1C569A9A:ZjM&1O6ds2EQ@14.225.17.176:5671/";
 
-        private string DomainApi = string.Empty;
-
-        private Queue<string> que_datas = new Queue<string>();
+        private string _api = string.Empty;
 
         private Thread polling_thread;
 
@@ -42,7 +40,6 @@ namespace BKSOFT.TVAN
 
         public void Start()
         {
-            DomainApi = ConfigurationManager.AppSettings["UrlAPI"];
             // Set flag
             is_running = true;
             // Create thread handle receive data
@@ -51,6 +48,45 @@ namespace BKSOFT.TVAN
             polling_thread.IsBackground = false;
             // Begin thread
             polling_thread.Start();
+            // Open queue
+            OpenTVanQueue();
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            try
+            {
+                var body = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                // Handler message
+                bool res = XMLHander(body);
+                if (res)
+                {
+                    this.PostSuc += 1;
+                }
+                else
+                {
+                    this.PostErr += 1;
+                }
+
+                //// Nack: thông báo trả lại message cho queue với trường hợp xử lý lỗi hoặc muốn xử lý sau, mess sẽ push lại queue
+                //channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: true);
+
+                //// Ack: thông báo đã xử lý message thành công và xóa khỏi queue
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            }
+            catch (Exception ex)
+            {
+                GPSFileLog.WriteLog(string.Empty, ex);
+            }
+        }
+
+        private void PollingHandler()
+        {
+            while (is_running)
+            {
+                Thread.Sleep(1000);
+            }
         }
 
         private bool OpenTVanQueue()
@@ -94,192 +130,9 @@ namespace BKSOFT.TVAN
             return res;
         }
 
-        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
-        {
-            try
-            {
-                var body = Encoding.UTF8.GetString(e.Body.ToArray());
-
-                // Push to queue handler
-                lock (que_datas)
-                {
-                    que_datas.Enqueue(body);
-                }
-
-                //// Nack: thông báo trả lại message cho queue với trường hợp xử lý lỗi hoặc muốn xử lý sau, mess sẽ push lại queue
-                //channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: true);
-
-                //// Ack: thông báo đã xử lý message thành công và xóa khỏi queue
-                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
-            }
-            catch (Exception ex)
-            {
-                GPSFileLog.WriteLog(string.Empty, ex);
-            }
-        }
-
-        private void PollingHandler()
-        {
-            // Open queue
-            OpenTVanQueue();
-
-            while (is_running)
-            {
-                if (que_datas.Count > 0)
-                {
-                    lock (que_datas)
-                    {
-                        var xML = que_datas.Dequeue();
-
-                        GPSFileLog.WriteLog(xML);
-
-                        try
-                        {
-                            if (string.IsNullOrEmpty(xML))
-                            {
-                                continue;
-                            }
-                            xML = xML.Trim();
-
-                            // Get Thông tin chung.
-                            TTChung info = GetTTChungFromXML(xML);
-                            if (string.IsNullOrEmpty(info.MTDiep))
-                            {
-                                continue;
-                            }
-
-                            // Push To Server GPHD
-                            Task<bool> task = HTTPHelper.TCTPostData(this.DomainApi, Utilities.Base64Encode(xML), info.MTDTChieu, info.MST);
-                            task.Wait();
-
-                            // Check status
-                            if(task.Result)
-                            {
-                                this.PostSuc += 1;
-                            }    
-                            else
-                            {
-                                this.PostErr += 1;
-                            }    
-
-                            // Add to log
-                            AddToLogTransfer(info, xML, task.Result);
-                            AddToTCTTransfer2(info, xML, task.Result);
-
-                            // Push to web test
-                            if (!string.IsNullOrWhiteSpace(info.MST) && (info.MST.Contains("0105987432-999") || info.MST.Contains("0105987432-998")))
-                            {
-                                string urlTest = "https://hoadon-da.pmbk.vn/api/ThongDiepPhanHoi/GetPhanHoiTuCQT";
-                                Task<bool> taskTest = HTTPHelper.TCTPostData(urlTest, Utilities.Base64Encode(xML), info.MTDTChieu, info.MST);
-                                taskTest.Wait();
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            GPSFileLog.WriteLog(string.Empty, ex);
-                        }
-                    }
-                }
-
-                Thread.Sleep(500);
-            }
-        }
-
         public uint GetMessageCount()
         {
             return channel.MessageCount(queueName);
-        }
-
-        private TTChung GetTTChungFromXML(string strXMl)
-        {
-            TTChung info = new TTChung();
-            try
-            {
-                strXMl = strXMl.Trim();
-
-                // Get Thông tin chung
-                byte[] bytes = Encoding.UTF8.GetBytes(strXMl);
-                using (MemoryStream ms = new MemoryStream(bytes))
-                {
-                    using (StreamReader reader = new StreamReader(ms))
-                    {
-                        XDocument xDoc = XDocument.Load(reader);
-                        info = xDoc.Descendants("TTChung")
-                                       .Select(x => new TTChung
-                                       {
-                                           PBan = x.Element(nameof(info.PBan)).Value,
-                                           MNGui = x.Element(nameof(info.MNGui)).Value,
-                                           MNNhan = x.Element(nameof(info.MNNhan)).Value,
-                                           MLTDiep = x.Element(nameof(info.MLTDiep)).Value,
-                                           MTDiep = x.Element(nameof(info.MTDiep)).Value,
-                                           MTDTChieu = x.Element(nameof(info.MTDTChieu)).Value,
-                                           MST = x.Element(nameof(info.MST)).Value
-                                       }).FirstOrDefault();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                GPSFileLog.WriteLog(string.Empty, ex);
-            }
-
-            return info;
-        }
-
-        private bool AddToLogTransfer(TTChung info, string Xml, bool status)
-        {
-            bool res = false;
-            try
-            {
-                // Write log
-                using (var db = new TCTTranferEntities())
-                {
-                    db.TIVans.Add(new TIVan
-                    {
-                        Id = Guid.NewGuid(),
-                        DateTime = DateTime.Now,
-                        MNGui = info.MNGui,
-                        MNNhan = info.MNNhan,
-                        MLTDiep = Convert.ToInt32(info.MLTDiep),
-                        MTDiep = info.MTDiep,
-                        MTDTChieu = info.MTDTChieu,
-                        MST = info.MST,
-                        DataXML = Xml,
-                        BinXML = Encoding.UTF8.GetBytes(Xml),
-                        Status = status
-                    });
-
-                    db.SaveChanges();
-                }
-                res = true;
-            }
-            catch (Exception ex)
-            {
-                GPSFileLog.WriteLog(string.Empty, ex);
-            }
-
-            return res;
-        }
-
-        private bool AddToTCTTransfer2(TTChung info, string Xml, bool status)
-        {
-            bool res = false;
-
-            try
-            {
-                using (var db = new TCTTranferEntities())
-                {
-                    db.usp_InsertMessage(DateTime.Now, info.MNGui, info.MNNhan, Convert.ToInt32(info.MLTDiep), info.MTDiep, info.MTDTChieu, info.MST, Xml, status);
-                }
-
-                res = true;
-            }
-            catch (Exception ex)
-            {
-                GPSFileLog.WriteLog(string.Empty, ex);
-            }
-
-            return res;
         }
 
         public void Dispose()
