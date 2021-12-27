b9ba8e6aa6e05ca7e5f513cb9ac37cc008b36100
thunh1710 <thunth.1710@gmail.com>
2021-12-27 16:35:19 +0700

    push all code

---
+++ DLL/Migrations/20211227092518_add-quyen-xac-thuc-su-dung.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace DLL.Migrations
+{
+    public partial class addquyenxacthucsudung : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ThaoTacs",
+                columns: new string[] { "ThaoTacId", "Ma", "Ten", "STT" },
+                values: new object[,]
+                {
+                {
+                    Guid.NewGuid().ToString(),
+                    "MNG_CONFIRM",
+                    "Xác thực sử dụng",
+                    7
+                },
+           });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
