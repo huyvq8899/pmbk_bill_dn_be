a6b09b708cd8eca3f443c0956795e640cd301b11
thunh1710 <thunth.1710@gmail.com>
2022-01-21 15:28:11 +0700

    push code sua thong diep gui + lap dieu chinh

--- Services/Helper/XmlHelper.cs
+++ Services/Helper/XmlHelper.cs
@@ -53,6 +53,7 @@ namespace Services.Helper
         public static TTChungThongDiep GetTTChungFromStringXML(string strXml)
         {
             TTChungThongDiep result = new TTChungThongDiep();
+            TTChungThongDiep rs = new TTChungThongDiep();
             byte[] encodedString = Encoding.UTF8.GetBytes(strXml);
             MemoryStream ms = new MemoryStream(encodedString);
             ms.Flush();
@@ -60,20 +61,19 @@ namespace Services.Helper
             using (StreamReader reader = new StreamReader(ms))
             {
                 XDocument xDoc = XDocument.Load(reader);
-                result = xDoc.Descendants("TTChung")
-                   .Select(x => new TTChungThongDiep
-                   {
-                       PBan = x.Element(nameof(result.PBan)).Value,
-                       MNGui = x.Element(nameof(result.MNGui)).Value,
-                       MNNhan = x.Element(nameof(result.MNNhan)).Value,
-                       MLTDiep = x.Element(nameof(result.MLTDiep)).Value,
-                       MTDiep = x.Element(nameof(result.MTDiep)).Value,
-                       MTDTChieu = x.Element(nameof(result.MTDTChieu)).Value,
-                       MST = x.Element(nameof(result.MST)).Value
-                   })
-                   .FirstOrDefault();
+                var res = xDoc.Descendants("TTChung").FirstOrDefault();
+                rs = new TTChungThongDiep
+                {
+                    PBan = res.Element(nameof(result.PBan)).Value,
+                    MNGui = res.Element(nameof(result.MNGui)).Value,
+                    MNNhan = res.Element(nameof(result.MNNhan)).Value,
+                    MLTDiep = res.Element(nameof(result.MLTDiep)).Value,
+                    MTDiep = res.Element(nameof(result.MTDiep)).Value,
+                    MTDTChieu = res.Element(nameof(result.MTDTChieu)).Value,
+                    MST = res.Element(nameof(result.MST)).Value
+                };
             }
-            return result;
+            return rs;
         }
 
         public static async Task<bool> InsertThongDiepNhanAsync(ThongDiepPhanHoiParams @params, Datacontext dataContext)
