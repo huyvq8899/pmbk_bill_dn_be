1df98ef7d40cb8d3d20e18b5ed825c14895fa0a5
AnhBH <jangkyavn@gmail.com>
2022-01-26 10:50:40 +0700

    add 8 % thue

--- Services/Helper/TextHelper.cs
+++ Services/Helper/TextHelper.cs
@@ -1051,7 +1051,7 @@ namespace ManagementServices.Helper
             }
             else
             {
-                if (value == "3.5" || value == "7")
+                if (value == "3.5" || value == "7" || value == "8")
                 {
                     return $"KHAC:{value}%";
                 }
@@ -1093,50 +1093,6 @@ namespace ManagementServices.Helper
             {
                 return thueGTGT + "%";
             }
-        }
-
-        /// <summary>
-        /// get thuế gtgt chung từ thuế chi tiết
-        /// </summary>
-        /// <param name="thueGTGTs"></param>
-        /// <returns></returns>
-        public static string GetThueChungFromChiTiet(DateTime ngayHoaDon, List<string> thueGTGTs)
-        {
-            var thueSos = thueGTGTs.Where(x => x != "KCT" && x != "KKKNT")
-                .Select(x => new HoaDonDienTuChiTietViewModel
-                {
-                    ThueGTGT = x
-                })
-                .ToList();
-
-            foreach (var item in thueSos)
-            {
-                if (item.ThueGTGT.Contains("KHAC"))
-                {
-                    var sKhacVal = item.ThueGTGT.Split(":")[1];
-                    item.ThueGTGT = sKhacVal.Replace(".", ",");
-                }
-            }
-
-            if (thueSos.Any())
-            {
-                var max = thueSos.Select(x => decimal.Parse(x.ThueGTGT)).Max();
-
-                /// trong khoảng 11/2021 -> 12/2021 nếu thuế = 3,5 hoặc 7 thì giảm 70%
-                if ((max == 3.5M || max == 7) &&
-                    (ngayHoaDon.Date.Month == 11 || ngayHoaDon.Date.Month == 12) &&
-                    ngayHoaDon.Date.Year == 2021)
-                {
-                    max = max * 100 / 70;
-                    return $"{max:G29}% * 70%";
-                }
-                else
-                {
-                    return $"{max:G29}%";
-                }
-            }
-
-            return "\\";
         }
 
         public static string Base64Encode(this string plainText)
