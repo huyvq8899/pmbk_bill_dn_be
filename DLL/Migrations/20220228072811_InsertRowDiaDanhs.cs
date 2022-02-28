f6271a66873d080100afe09b36bea138038633d7
Van Lung <vlung05@gmail.com>
2022-02-28 15:06:15 +0700

    Thêm migratetion add row

---
+++ DLL/Migrations/20220228072811_InsertRowDiaDanhs.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class InsertRowDiaDanhs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT [dbo].[DiaDanhs] ([Id], [Ma], [Ten]) VALUES (N'FC89062F-8825-4AF6-9961-E30265845E22', N'65', N'Cục Thuế Doanh nghiệp lớn')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
