fc88cabb1408120205e1746bd76273ad66f85831
AnhBH <jangkyavn@gmail.com>
2022-03-24 13:46:50 +0700

    Feature #587: Soát hoàn thiện hoadon-da 16-03-2022

---
+++ DLL/Migrations/20220324062945_change-copyright-email.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DLL.Migrations
+{
+    public partial class changecopyrightemail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ConfigNoiDungEmails
+                SET NoiDungEmail = REPLACE(NoiDungEmail, '2020 PHAN', ' 2021 - 2022 PHAN');
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
