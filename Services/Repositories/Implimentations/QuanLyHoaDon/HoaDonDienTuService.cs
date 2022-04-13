69c1f9ae752809e6a6b08ec26d1777c1d6fbd99c
AnhBH <jangkyavn@gmail.com>
2022-04-13 07:34:58 +0700

    remove log

--- Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
+++ Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
@@ -3621,8 +3621,6 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                     pdfFileName = $"{hd.BoKyHieuHoaDon.KyHieu}-{hd.SoHoaDon}-{Guid.NewGuid()}.pdf";
                     entity.FileDaKy = pdfFileName;
 
-                    Tracert.WriteLog("TestHost: " + _IHttpContextAccessor.HttpContext.Request.Host.ToString());
-
                     if (hd.IsCapMa == true)
                     {
                         xmlFileName = $"{hd.BoKyHieuHoaDon.KyHieu}-{hd.SoHoaDon}-{Guid.NewGuid()}.xml";
