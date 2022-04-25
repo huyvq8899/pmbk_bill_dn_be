6dd7af5aebcee7b1e5e757751d2cdf0f95d417df
AnhBH <jangkyavn@gmail.com>
2022-04-25 17:25:53 +0700

    test send background ok

--- API/Services/ScopedProcessingService.cs
+++ API/Services/ScopedProcessingService.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DLL;
+using ManagementServices.Helper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Services.Helper;
+using Services.Repositories.Interfaces;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,10 +23,16 @@ namespace API.Services
     {
         private int executionCount = 0;
         private readonly ILogger _logger;
+        private readonly IDatabaseService _databaseService;
+        private readonly IConfiguration _configuration;
 
-        public ScopedProcessingService(ILogger<ScopedProcessingService> logger)
+        public ScopedProcessingService(ILogger<ScopedProcessingService> logger,
+            IDatabaseService databaseService,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _databaseService = databaseService;
+            _configuration = configuration;
         }
 
         public async Task DoWork(CancellationToken stoppingToken)
@@ -30,10 +44,66 @@ namespace API.Services
                 _logger.LogInformation(
                     "Scoped Processing Service is working. Count: {Count}", executionCount);
 
-                Tracert.WriteLog("Test: " + DateTime.Now.ToString("HH:mm:ss"));
+                // await DoSendHoaDonKhongMaToCQTAsync();
 
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        /// <summary>
+        /// Do Send hóa đơn không mã đến cơ quan thuế
+        /// </summary>
+        /// <returns></returns>
+        public async Task DoSendHoaDonKhongMaToCQTAsync()
+        {
+            if (DateTime.Now.ToString("HH:mm:ss") == "17:25:00")
+            {
+                var companies = await _databaseService.GetCompanies();
+                companies = companies.Where(x => x.DataBaseName == "UAT0200784873998Invoice").ToList();
+
+                var tasks = new List<Task>();
+
+                foreach (var item in companies)
+                {
+                    tasks.Add(SendAPIGuiThongDiepDuLieuHDDTBackground(item));
+                }
+
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        /// <summary>
+        /// Send hóa đơn không mã đến cơ quan thuế
+        /// </summary>
+        /// <param name="companyModel"></param>
+        /// <returns></returns>
+        public async Task SendAPIGuiThongDiepDuLieuHDDTBackground(CompanyModel companyModel)
+        {
+            var keyParams = new KeyParams
+            {
+                KeyString = companyModel.ConnectionString.Base64Encode(),
+                DatabaseName = companyModel.DataBaseName
+            };
+
+            using (var client = new HttpClient())
+            {
+                string url = _configuration["Config:Domain"];
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res = await client.PostAsJsonAsync("/api/ThongDiepGuiDuLieuHDDT/GuiThongDiepDuLieuHDDTBackground", keyParams);
+
+                if (!res.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Send Successfully");
+                }
+                else
+                {
+                    Tracert.WriteLog("FailGuiThongDiepDuLieuHDDTBackground: " + companyModel.DataBaseName);
+                }
+            }
+
         }
     }
 }
