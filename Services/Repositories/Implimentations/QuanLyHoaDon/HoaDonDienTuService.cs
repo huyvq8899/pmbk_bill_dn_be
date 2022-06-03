e7afd1e82900496276ac2e20e85352fe1881864e
thunth <thunth.1710@gmail.com>
2022-06-03 16:04:02 +0700

    push code sua truong hop dieu chinh thong bao sai sot

--- Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
+++ Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
@@ -13400,7 +13400,7 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                                                         }
                                                         else
                                                         {
-                                                            dienGiaiTrangThaiGui = trangThaiGuiThongDiep.GetDescription();
+                                                            dienGiaiTrangThaiGui = trangThai.GetDescription();
                                                         }
                                                         var lanGui = thongDiepChiTiets.Count(x => x.HoaDonDienTuId == item.HoaDonDienTuId && x.ChungTuLienQuan == item.ChungTuLienQuan && x.PhanLoaiHDSaiSot == item.PhanLoaiHDSaiSot);
 
@@ -13417,44 +13417,6 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                                                         };
                                                     }
                                                 }
-                                            }
-                                            else
-                                            {
-                                                var thongDiep = thongDiepChungs.FirstOrDefault(x => x.IdThamChieu == item.ThongDiepGuiCQTId);
-                                                var trangThai = (TrangThaiGuiThongDiep)thongDiep.TrangThaiGui;
-                                                if (trangThai == TrangThaiGuiThongDiep.CoHDKhongHopLe || trangThaiGuiThongDiep == TrangThaiGuiThongDiep.GoiDuLieuKhongHopLe)
-                                                {
-                                                    dienGiaiTrangThaiGui = "Hóa đơn không hợp lệ";
-                                                }
-                                                else if (trangThai == TrangThaiGuiThongDiep.CoHoaDonCQTKhongTiepNhan)
-                                                {
-                                                    dienGiaiTrangThaiGui = "CQT không tiếp nhận";
-                                                }
-                                                else if (trangThai == TrangThaiGuiThongDiep.CQTTiepNhanTatCaHoaDon)
-                                                {
-                                                    dienGiaiTrangThaiGui = "CQT đã tiếp nhận";
-                                                }
-                                                else if (trangThai == TrangThaiGuiThongDiep.GoiDuLieuHopLe)
-                                                {
-                                                    dienGiaiTrangThaiGui = "Hóa đơn hợp lệ";
-                                                }
-                                                else
-                                                {
-                                                    dienGiaiTrangThaiGui = trangThai.GetDescription();
-                                                }
-
-                                                var lanGui = thongDiepChiTiets.Count(x => x.HoaDonDienTuId == item.HoaDonDienTuId && x.ChungTuLienQuan == item.ChungTuLienQuan && x.PhanLoaiHDSaiSot == item.PhanLoaiHDSaiSot);
-
-                                                return new CotThongBaoSaiSotViewModel
-                                                {
-                                                    TrangThaiLapVaGuiThongBao = thongDiep.TrangThaiGui,
-                                                    ThongDiepGuiCQTId = item.ThongDiepGuiCQTId,
-                                                    TenTrangThai = dienGiaiTrangThaiGui,
-                                                    DienGiaiChiTietTrangThai = dienGiaiTrangThaiGui,
-                                                    LanGui = "Lần gửi " + lanGui.ToString(),
-                                                    IsTrongHan = (hienThiTinhTrang) ? ((bool?)((thongDiep.TrangThaiGui.GetValueOrDefault() > -1))) : null
-                                                    //IsTrongHan = XacDinhTrongHan(tuyChonKyKeKhai, hoaDon, boKyHieuHoaDon, listHoaDonDienTu)
-                                                };
                                             }
                                         }
                                     }
@@ -13501,7 +13463,7 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                                             {
                                                 HoaDonDienTuId = hoaDonBiDieuChinh.HoaDonDienTuId,
                                                 ThongDiepGuiCQTId = item.ThongDiepGuiCQTId,
-                                                TrangThaiLapVaGuiThongBao = hoaDon.TrangThaiGui04.GetValueOrDefault(),
+                                                TrangThaiLapVaGuiThongBao = thongBao.TrangThaiGui.GetValueOrDefault(),
                                                 TenTrangThai = dienGiaiTrangThaiGui,
                                                 DienGiaiChiTietTrangThai = dienGiaiTrangThaiGui,
                                                 LanGui = "Lần gửi " + lanGui.ToString(),
