fba877e4ed72ab109ad68b1362ab6cfc398bd3be
thunth <thunth.1710@gmail.com>
2022-06-04 07:43:26 +0700

    sua loi gui thong diep cqt

--- Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
+++ Services/Repositories/Implimentations/QuanLyHoaDon/HoaDonDienTuService.cs
@@ -1419,7 +1419,9 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                                                      join tdc in _db.ThongDiepChungs on bthdlhd.ThongDiepChungId equals tdc.ThongDiepChungId
                                                      where bthdlhdct.MauSo == bkhhd.KyHieuMauSoHoaDon && bthdlhdct.KyHieu == bkhhd.KyHieuHoaDon && bthdlhdct.SoHoaDon == hd.SoHoaDon && tdc.ThongDiepGuiDi == true
                                                      orderby tdc.NgayGui descending, tdc.CreatedDate descending
-                                                     select $"Số {bthdlhd.SoBTHDLieu}/{(bthdlhd.LanDau == true ? "Lần đầu" : $"Bổ sung lần thứ {bthdlhd.BoSungLanThu}")}/{((TrangThaiGuiThongDiep)tdc.TrangThaiGui).GetDescription()}").FirstOrDefault()
+                                                     select $"Số {bthdlhd.SoBTHDLieu}/{(bthdlhd.LanDau == true ? "Lần đầu" : $"Bổ sung lần thứ {bthdlhd.BoSungLanThu}")}/{((TrangThaiGuiThongDiep)tdc.TrangThaiGui).GetDescription()}").FirstOrDefault(),
+                            ThongDiepGuiCQTId = _db.ThongDiepChiTietGuiCQTs.Where(x=>x.HoaDonDienTuId == hd.HoaDonDienTuId).OrderByDescending(x=>x.CreatedDate).Select(x=>x.ThongDiepGuiCQTId).FirstOrDefault()
+                            
                         };
 
             var result = await query.FirstOrDefaultAsync();
@@ -13513,7 +13515,7 @@ namespace Services.Repositories.Implimentations.QuanLyHoaDon
                                     ThongDiepGuiCQTId = hoaDonBiDieuChinh.ThongDiepGuiCQTId,
                                     TrangThaiLapVaGuiThongBao = -2, //chưa lập thông báo
                                     TenTrangThai = "Chưa lập thông báo",
-                                    DienGiaiChiTietTrangThai = "&nbsp;|&nbsp;Hóa đơn gốc bị điều chỉnh",
+                                    DienGiaiChiTietTrangThai = "&nbsp;|&nbsp;Hóa đơn gốc bị điều chỉnh",GetById
                                     IsTrongHan = ((daDuocCapMa) ? XacDinhTrongHan(tuyChonKyKeKhai, hoaDon, boKyHieuHoaDon, listHoaDonDienTu) : null),
                                     IsHoaDonDieuChinh = true //là hóa đơn điều chỉnh vì dòng thông báo sai sót này nằm ở hóa đơn điều chỉnh
                                 };
