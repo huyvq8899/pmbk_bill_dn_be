1e14ddb1fe32575e1fe452c25fe8ecf8161645ed
thunth <thunth.1710@gmail.com>
2022-06-23 15:39:41 +0700

    sua link cusman

--- Services/Repositories/Implimentations/DatabaseService.cs
+++ Services/Repositories/Implimentations/DatabaseService.cs
@@ -353,7 +353,7 @@ namespace Services.Repositories.Implimentations
             // Get connection string default database.
             if(formatConnection.Contains("Database={0}"))
             {
-                return string.Format(formatConnection, "CusManKiemThu");
+                return string.Format(formatConnection, "CusMan");
             }
 
             return formatConnection;
