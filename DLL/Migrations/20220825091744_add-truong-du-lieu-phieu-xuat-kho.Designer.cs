5ff678456a3c5891c1116065df758837be3e384c
AnhBH <jangkyavn@gmail.com>
2022-08-25 16:58:45 +0700

    pending add phieu xuat kho to kh

--- DLL/Migrations/20220825091744_add-truong-du-lieu-phieu-xuat-kho.Designer.cs
+++ DLL/Migrations/20220825091744_add-truong-du-lieu-phieu-xuat-kho.Designer.cs
@@ -4,14 +4,16 @@ using DLL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DLL.Migrations
 {
     [DbContext(typeof(Datacontext))]
-    partial class DatacontextModelSnapshot : ModelSnapshot
+    [Migration("20220825091744_add-truong-du-lieu-phieu-xuat-kho")]
+    partial class addtruongdulieuphieuxuatkho
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
