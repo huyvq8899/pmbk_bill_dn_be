bc08233d35c17dd6fd8198ad9b8ade31a9f716b4
thunh <thunth.1710@gmail.com>
2022-11-11 16:57:37 +0700

    push code sua thue suat

--- Services/Repositories/Implimentations/XMLInvoiceService.cs
+++ Services/Repositories/Implimentations/XMLInvoiceService.cs
@@ -627,12 +627,15 @@ namespace Services.Repositories.Implimentations
 
                     foreach (var item in groupThueGTGT)
                     {
-                        hDonGTGT.DLHDon.NDHDon.TToan.THTTLTSuat.Add(new ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.a.LTSuat
+                        if (!string.IsNullOrEmpty(item.ThueGTGT))
                         {
-                            TSuat = item.ThueGTGT,
-                            ThTien = item.ThanhTien ?? 0,
-                            TThue = item.TienThueGTGT,
-                        });
+                            hDonGTGT.DLHDon.NDHDon.TToan.THTTLTSuat.Add(new ViewModels.XML.QuyDinhKyThuatHDDT.PhanII.II._2.a.LTSuat
+                            {
+                                TSuat = item.ThueGTGT,
+                                ThTien = item.ThanhTien ?? 0,
+                                TThue = item.TienThueGTGT,
+                            });
+                        }
                     }
                     #endregion
 
